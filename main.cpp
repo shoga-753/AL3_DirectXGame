34a25fbe70b1c4cdd901cacb4930acf5579d5f21
shoga-753 <k022g1050@g.neec.ac.jp>
2023-09-19 10:54:13 +0900

    名前変更

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow(L"GC2C_06_サイトウ_ショウマ_AL3");
+	win->CreateGameWindow(L"3DSTG");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
